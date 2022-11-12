3c47569d8648a8d7263edbd633877094a393f901
Jost Baron <j.baron@netzkoenig.de>
2022-11-12 15:23:40 +0100

    [BUGFIX] Make driver registration work with TYPO3 11.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,16 +3,12 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-/** @var \TYPO3\CMS\Core\Resource\Driver\DriverRegistry $driverRegistry */
-$driverRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-    \TYPO3\CMS\Core\Resource\Driver\DriverRegistry::class
-);
-$driverRegistry->registerDriverClass(
-    \Jbaron\FalDatabase\Driver\DatabaseDriver::class,
-    \Jbaron\FalDatabase\Driver\DatabaseDriver::DRIVER_KEY,
-    'Database driver for FAL',
-    'FILE:EXT:fal_database/Configuration/FlexForm/DriverFlexForm.xml'
-);
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['registeredDrivers'][] = [
+    'class' => \Jbaron\FalDatabase\Driver\DatabaseDriver::class,
+    'shortName' => \Jbaron\FalDatabase\Driver\DatabaseDriver::DRIVER_KEY,
+    'label' => 'Database driver for FAL',
+    'flexFormDS' => 'FILE:EXT:fal_database/Configuration/FlexForm/DriverFlexForm.xml',
+];
 
 if (!\is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_jbaron_faldatabase_existencecache'])) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_jbaron_faldatabase_existencecache'] = [];
